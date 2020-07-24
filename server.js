3f296473d1e6f0718c8a57f58bceef4cd7b79fb6
Dwi Septiana Maulida <dseptiana07.dsm@gmail.com>
2020-07-24 15:53:14 +0700

    package

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require('express');const app = express();app.use(express.static('./dist/katagonia-frontend'));app.get('/*', (req, res) =>    res.sendFile('index.html', {root: 'dist/katagonia-frontend/'}),);app.listen(process.env.PORT || 8080);
